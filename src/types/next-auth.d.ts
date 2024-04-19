19e74c7d307bda90cd1fcf72d5469d80749df792
Hamza <mhumza13420@gmail.com>
2024-04-19 11:02:23 -0700

    nextAuth sign-in

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+import "next-auth"
+import { DefaultSession } from "next-auth";
+
+// change data type of this module || add new fields
+
+declare module 'next-auth' {
+    interface User {
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessages?: boolean;
+        username?: string
+    }
+    interface Session {
+        user: {
+            _id?: string;
+            isVerified?: boolean;
+            isAcceptingMessages?: boolean;
+            username?: string
+        } & DefaultSession ['user']
+    } 
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessages?: boolean;
+        username?: string
+    }
+}
