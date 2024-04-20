df81e278666bf52b93889606b947cf9585f7b589
Hamza <mhumza13420@gmail.com>
2024-04-20 02:51:41 -0700

    check username unique

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -14,10 +14,10 @@ async function dbConnect (): Promise<void> {
 
     try {
         const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
-        console.log("DB: ", db)
+        // console.log("DB: ", db)
 
         connection.isConnected = db.connections[0].readyState
-        console.log("DB.Connection: ", db.connections)
+        // console.log("DB.Connection: ", db.connections)
         console.log("DB Connected Successfully")
 
     } catch (error) {
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -3,7 +3,7 @@ import {z} from "zod"
 export const usernameValidation = z
     .string()
     .min(2, "Username must be at least 2 characters")
-    .min(20, "Username no more than 20 characters")
+    .max(20, "Username no more than 20 characters")
     .regex(/^^[a-zA-Z0-9_]+$/, "Username not contain specail character")
 
 export const signUpSchema = z.object({
